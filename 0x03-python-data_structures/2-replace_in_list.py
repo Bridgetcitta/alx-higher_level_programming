8e95b9e9be68cf6aaf68be981d7764d29b370ff3
Bridgetcitta <oforibridget20@gmail.com>
2023-10-10 16:11:24 -0700

    Updated files: 0x02-python-import_modules/102-magic_calculation.py 0x03-python-data_structures/2-replace_in_list.py

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -2,5 +2,6 @@
 
 def replace_in_list(my_list, idx, element):
     """replace elements of a list"""
-    if idx < 0 or idx > (len(my_list) - 1):
-        return (my_list, idx, element)
+    if idx >= 0 and idx < len(my_list):
+        my_list[idx] = element
+    return (my_list)
