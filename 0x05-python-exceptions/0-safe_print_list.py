8602dfa9b2f4d5e4bc1edfcca78595be6965e9a4
Bridgetcitta <oforibridget20@gmail.com>
2023-10-24 14:55:59 -0700

    Updated files: 0x05-python-exceptions/0-safe_print_list.py

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -9,7 +9,7 @@ def safe_print_list(my_list=[], x=0):
                 break
             print(i, end=' ')
             count += 1
+        print (' ')
+        return count
     except Exception:
         pass
-        print()
-        return (count)
