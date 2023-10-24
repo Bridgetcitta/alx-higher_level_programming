832bd2bfb121a8e0ca7dc9de7437dcaac6b459c5
Bridgetcitta <oforibridget20@gmail.com>
2023-10-24 15:08:07 -0700

    Updated files: 0x05-python-exceptions/0-safe_print_list.py

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -2,14 +2,12 @@
 
 def safe_print_list(my_list=[], x=0):
 
-    try:
-        count 0
-        for i in my_list:
-            if count >= x:
-                break
-            print(i, end=' ')
+    count = 0
+    for i in range(x):
+        try:
+            print(my_list[i], end=" ")
             count += 1
-        print (' ')
+        except IndexError:
+            break
+        print(" ")
         return count
-    except Exception:
-        pass
