ea171a34c3d4be008c9a043d764fe13d2051328c
Bridgetcitta <oforibridget20@gmail.com>
2024-02-15 14:21:29 -0800

    new

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -2,5 +2,4 @@
 -- Table = first_table
 USE `hbtn_0c_0`
 ALTER TABLE `first_table`
-CONVERT TO CHARACTER SET utf8mb4, COLLATE utf8mb4_unicode_ci;
-
+CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
